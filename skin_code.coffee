b91cc4cc19bdd74d7183b8bbc505c5578232ec30
apeckham <apeckham@gmail.com>
2013-12-09 00:03:57 -0800

    use anchors instead of a div

--- skin_code.coffee
+++ skin_code.coffee
@@ -1,11 +1,8 @@
 window.ba_clickable_ad_bg = ->
-  ba_winWidth = $parent.find("body").width()
   ba_contentOffset = $parent.find("#outer").offset()
-  ba_leftWidth = ba_contentOffset.left
-  ba_rightWidth = ba_winWidth - (ba_contentOffset.left + $parent.find("#outer").width() + parseInt($parent.find("#outer").css("padding-left")) + parseInt($parent.find("#outer").css("padding-right")))
 
   $parent.find("body").prepend """
-    <div id="ba_clickable_bg_left" class="hidden-phone" style="display: none;"><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/></div>
+    <a id="ba_clickable_bg_left"><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/></a>
   """
   $parent.find("#ba_clickable_bg_left").css "width", "160px"
   $parent.find("#ba_clickable_bg_left").css "height", "610px"
@@ -22,13 +19,8 @@ window.ba_clickable_ad_bg = ->
   $parent.find("#ba_clickable_bg_left").click ->
     window.open dfpMacros.clickUrl
 
-  if ba_leftWidth > 0
-    $parent.find("#ba_clickable_bg_left").show()
-  else
-    $parent.find("#ba_clickable_bg_left").hide()
-
   $parent.find("body").prepend """
-    <div id="ba_clickable_bg_right" class="hidden-phone" style="display: none;"><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/></div>
+    <a id="ba_clickable_bg_right"><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/></a>
   """
   $parent.find("#ba_clickable_bg_right").css "width", "160px"
   $parent.find("#ba_clickable_bg_right").css "height", "610px"
@@ -44,11 +36,6 @@ window.ba_clickable_ad_bg = ->
   $parent.find("#ba_clickable_bg_right").unbind()
   $parent.find("#ba_clickable_bg_right").click ->
     window.open dfpMacros.clickUrl
-
-  if ba_rightWidth > 0
-    $parent.find("#ba_clickable_bg_right").show()
-  else
-    $parent.find("#ba_clickable_bg_right").hide()
 
 $parent = jQuery(window.parent.document)
 $parent.find("head").append """
--- skin_code.js
+++ skin_code.js
@@ -3,12 +3,9 @@
   var $parent;
 
   window.ba_clickable_ad_bg = function() {
-    var ba_contentOffset, ba_leftWidth, ba_rightWidth, ba_winWidth;
-    ba_winWidth = $parent.find("body").width();
+    var ba_contentOffset;
     ba_contentOffset = $parent.find("#outer").offset();
-    ba_leftWidth = ba_contentOffset.left;
-    ba_rightWidth = ba_winWidth - (ba_contentOffset.left + $parent.find("#outer").width() + parseInt($parent.find("#outer").css("padding-left")) + parseInt($parent.find("#outer").css("padding-right")));
-    $parent.find("body").prepend("<div id=\"ba_clickable_bg_left\" class=\"hidden-phone\" style=\"display: none;\"><img src=\"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==\"/></div>");
+    $parent.find("body").prepend("<a id=\"ba_clickable_bg_left\"><img src=\"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==\"/></a>");
     $parent.find("#ba_clickable_bg_left").css("width", "160px");
     $parent.find("#ba_clickable_bg_left").css("height", "610px");
     $parent.find("#ba_clickable_bg_left img").css("width", "160px");
@@ -24,12 +21,7 @@
     $parent.find("#ba_clickable_bg_left").click(function() {
       return window.open(dfpMacros.clickUrl);
     });
-    if (ba_leftWidth > 0) {
-      $parent.find("#ba_clickable_bg_left").show();
-    } else {
-      $parent.find("#ba_clickable_bg_left").hide();
-    }
-    $parent.find("body").prepend("<div id=\"ba_clickable_bg_right\" class=\"hidden-phone\" style=\"display: none;\"><img src=\"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==\"/></div>");
+    $parent.find("body").prepend("<a id=\"ba_clickable_bg_right\"><img src=\"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==\"/></a>");
     $parent.find("#ba_clickable_bg_right").css("width", "160px");
     $parent.find("#ba_clickable_bg_right").css("height", "610px");
     $parent.find("#ba_clickable_bg_right img").css("width", "160px");
@@ -42,14 +34,9 @@
     $parent.find("#ba_clickable_bg_right").css("top", "0px");
     $parent.find("#ba_clickable_bg_right").css("left", (ba_contentOffset.left + $parent.find("#outer").width()) + "px");
     $parent.find("#ba_clickable_bg_right").unbind();
-    $parent.find("#ba_clickable_bg_right").click(function() {
+    return $parent.find("#ba_clickable_bg_right").click(function() {
       return window.open(dfpMacros.clickUrl);
     });
-    if (ba_rightWidth > 0) {
-      return $parent.find("#ba_clickable_bg_right").show();
-    } else {
-      return $parent.find("#ba_clickable_bg_right").hide();
-    }
   };
 
   $parent = jQuery(window.parent.document);
